1ad973b2b498a1dd81f8b3d83a67e625d485112f
Santosh Thoduka <santosh.thoduka@gmail.com>
2019-07-12 18:44:01 +0200

    add record and pick

---
+++ mdr_planning/mdr_behaviours/mdr_common_behaviours/ros/src/mdr_common_behaviours/start_recording.py
@@ -0,0 +1,34 @@
+from mas_execution_manager.scenario_state_base import ScenarioStateBase
+import std_msgs
+import rospy
+import time
+
+class StartRecording(ScenarioStateBase):
+    def __init__(self, save_sm_state=False, **kwargs):
+        ScenarioStateBase.__init__(self, 'start_recording',
+                                   save_sm_state=save_sm_state,
+                                   outcomes=['succeeded', 'failed'])
+        self.sm_id = kwargs.get('sm_id', '')
+        self.state_name = kwargs.get('state_name', 'start_recording')
+        self.timeout = kwargs.get('timeout', 8.)
+        self.event_pub = rospy.Publisher('/mcr_tools/rosbag_recorder/event_in', std_msgs.msg.String, 1)
+        self.event_sub = rospy.Subscriber('/mcr_tools/rosbag_recorder/event_out', std_msgs.msg.String, self.eventCallback)
+        self.msg = None
+
+    def eventCallback(self, msg):
+        self.msg = msg
+
+    def execute(self, userdata):
+        self.msg = None
+        self.event_pub.publish('e_start')
+        start_time = time.time()
+        duration = 0.
+        while duration < self.timeout:
+            rospy.sleep(0.1)
+            if self.msg is not None:
+                break
+            duration = time.time() - start_time
+        if self.msg is not None and self.msg.data == 'e_started':
+            return 'succeeded'
+        else
+            return 'failed'
